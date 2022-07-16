0aebcac0f836daf2e6752b3c5c738920493e36d2
kapich1192 <kapich1192@yandex.ru>
2022-07-16 16:34:34 +0300

    developing

---
+++ src/main/java/com/code/models/article/Article.java
@@ -0,0 +1,14 @@
+package com.code.models.article;
+
+public class Article{
+
+    private Long id;
+
+    private String title;
+    private String description;
+    private String full_text;
+    /*constructors*/
+
+    /*getters*/
+    /*setters*/
+}
---
+++ src/main/java/com/code/models/user/UserModel.java
@@ -0,0 +1,4 @@
+package com.code.models.user;
+
+public class UserModel {
+}
