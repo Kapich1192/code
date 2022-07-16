05d857d03e40c0f3733cdec5627bc7c171962c81
kapich1192 <kapich1192@yandex.ru>
2022-07-16 16:36:57 +0300

    developing

--- src/main/java/com/code/models/article/Article.java
+++ src/main/java/com/code/models/article/Article.java
@@ -1,14 +1,26 @@
 package com.code.models.article;
 
 public class Article{
-
     private Long id;
-
     private String title;
     private String description;
+    private String anons;
     private String full_text;
     /*constructors*/
-
+    public Article() {}
+    public Article(String title) {
+        this.title = title;
+    }
     /*getters*/
+    public Long getId() {return id;}
+    public String getTitle() {return title;}
+    public String getDescription() {return description;}
+    public String getFull_text() {return full_text;}
+    public String getAnons() {return anons;}
     /*setters*/
+    public void setId(Long id) {this.id = id;}
+    public void setTitle(String title) {this.title = title;}
+    public void setDescription(String description) {this.description = description;}
+    public void setFull_text(String full_text) {this.full_text = full_text;}
+    public void setAnons(String anons) {this.anons = anons;}
 }
