abe173391b39ce5fbf564df1288b55ac9f677c17
kapich1192 <kapich1192@yandex.ru>
2022-07-23 15:59:38 +0300

    add_sec_fininish

--- src/main/java/com/code/models/user/Role.java
+++ src/main/java/com/code/models/user/Role.java
@@ -1,4 +1,5 @@
 package com.code.models.user;
 
 public enum Role {
+    USER;
 }
--- src/main/java/com/code/models/user/User.java
+++ src/main/java/com/code/models/user/User.java
@@ -13,7 +13,6 @@ public class User {
     private String username;
     private String password;
     private boolean active;
-
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER )
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
